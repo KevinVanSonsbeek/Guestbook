714c18ed389989d17ada64b09430f23decd3f233
Kevin van Sonsbeek <kevin_787@msn.com>
2021-06-15 16:27:50 +0200

    Show page.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,6 +27,21 @@ class ConferenceController
         return new Response($this->twig->render(
             'conference/index.html.twig', [
                 'conferences' => $conferences,
+            ]
+        ));
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(
+        Conference $conference,
+        CommentRepository $commentRepository
+    ): Response {
+        $comments = $commentRepository->findBy(['conference' => $conference]);
+
+        return new Response($this->twig->render(
+            'conference/show.html.twig', [
+                'conference' => $conference,
+                'comments' => $comments,
             ]
         ));
     }
