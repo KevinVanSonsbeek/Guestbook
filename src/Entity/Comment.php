4dbc976636ea62b2446d89226be76ace25534d09
Kevin van Sonsbeek <kevin_787@msn.com>
2021-06-15 12:52:05 +0200

    Added photoFilename to comment.

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -46,6 +46,11 @@ class Comment
      */
     private $conference;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $photoFilename;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -107,6 +112,18 @@ class Comment
     public function setConference(?Conference $conference): self
     {
         $this->conference = $conference;
+
+        return $this;
+    }
+
+    public function getPhotoFilename(): ?string
+    {
+        return $this->photoFilename;
+    }
+
+    public function setPhotoFilename(?string $photoFilename): self
+    {
+        $this->photoFilename = $photoFilename;
 
         return $this;
     }
