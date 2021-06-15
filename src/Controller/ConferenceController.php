dc4c02d16d672a345215832461f4a19084650fa2
Kevin van Sonsbeek <kevin_787@msn.com>
2021-06-15 14:37:08 +0200

    Conferences index page.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -12,17 +13,18 @@ class ConferenceController
 {
     public function __construct(
         private Environment $twig,
+        private ConferenceRepository $conferenceRepository
     ) {
     }
 
-    #[Route('/{name?World}', name: 'homepage')]
-    public function index(
-        string $name
-    ): Response {
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        $conferences = $this->conferenceRepository->findAll();
+
         return new Response($this->twig->render(
             'conference/index.html.twig', [
-                'controller_name' => 'ConferenceController',
-                'name' => $name,
+                'conferences' => $conferences,
             ]
         ));
     }
