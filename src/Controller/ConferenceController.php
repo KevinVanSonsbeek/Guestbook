7bcd03c2ba690861a02e2a90970778782b0da634
Kevin van Sonsbeek <kevin_787@msn.com>
2021-06-15 16:35:34 +0200

    Implemented pagination for the comments.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -33,15 +34,19 @@ class ConferenceController
 
     #[Route('/conference/{id}', name: 'conference')]
     public function show(
+        Request $request,
         Conference $conference,
         CommentRepository $commentRepository
     ): Response {
-        $comments = $commentRepository->findBy(['conference' => $conference]);
+        $offset = max(0, $request->query->getInt('offset'));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
         return new Response($this->twig->render(
             'conference/show.html.twig', [
                 'conference' => $conference,
-                'comments' => $comments,
+                'comments' => $paginator,
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
             ]
         ));
     }
