89bd56d7d685050892608c9921d6ef3fd6fbadfc
Kevin van Sonsbeek <kevin_787@msn.com>
2021-06-16 16:02:48 +0200

    Added comment submit test.

--- tests/Functional/Controller/ConferenceControllerTest.php
+++ tests/Functional/Controller/ConferenceControllerTest.php
@@ -31,4 +31,21 @@ final class ConferenceControllerTest extends WebTestCase
         self::assertSelectorTextContains('h2', 'Amsterdam 2019');
         self::assertSelectorExists('div:contains("There are 1 comments")');
     }
+
+    public function testCommentSubmission()
+    {
+        $client = self::createClient();
+        $client->request('GET', '/');
+        $client->clickLink('View');
+        
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'Fabien',
+            'comment_form[text]' => 'Some feedback from an automated functional test',
+            'comment_form[email]' => 'me@automat.ed',
+            'comment_form[photo]' => dirname(__DIR__, 2) . '/public/images/under-construction.gif',
+        ]);
+        self::assertResponseRedirects();
+        $client->followRedirect();
+        self::assertSelectorExists('div:contains("There are 2 comments")');
+    }
 }
