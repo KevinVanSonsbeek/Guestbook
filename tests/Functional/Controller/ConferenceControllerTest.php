36d047a28719b1d90bb212d31b0cc1649d795070
Kevin van Sonsbeek <kevin_787@msn.com>
2021-06-16 15:58:21 +0200

    Added new functional test and changed show page a bit.

--- tests/Functional/Controller/ConferenceControllerTest.php
+++ tests/Functional/Controller/ConferenceControllerTest.php
@@ -16,4 +16,19 @@ final class ConferenceControllerTest extends WebTestCase
         self::assertResponseIsSuccessful();
         self::assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+    public function testConferencePage()
+    {
+        $client = self::createClient();
+        $crawler = $client->request('GET', '/');
+
+        self::assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        self::assertPageTitleContains('Amsterdam');
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h2', 'Amsterdam 2019');
+        self::assertSelectorExists('div:contains("There are 1 comments")');
+    }
 }
